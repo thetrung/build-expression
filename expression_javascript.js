964fda3707324502306ac79055bd76a5a162cd1b
Nguyễn Thế Trung <deulamco@gmail.com>
2021-10-15 20:46:12 +0700

    Delete expression_javascript.js

--- expression_javascript.js
+++
@@ -1,9 +0,0 @@
-let Number   = (n) => (env) => n
-let Variable = (s) => (env) => env[s]
-let Add      = (e1, e2) => (env) => e1(env) + e2(env)
-let Multiply = (e1, e2) => (env) => e1(env) * e2(env)
-
-let environment = {a: 3, b: 4}
-let expression  = Add(Variable('a'), Multiply(Number(2), Variable('b')))
-
-console.log(expression(environment))
