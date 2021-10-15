1c863ced50a23ca043f3c470c433e18708c18d33
Nguyễn Thế Trung <deulamco@gmail.com>
2021-10-15 20:38:19 +0700

    Delete expression_clojure2.clj

--- expression_clojure2.clj
+++
@@ -1,8 +0,0 @@
-(defn Add [x y] #(+ (x %) (y %)))
-(defn Mul [x y] #(* (x %) (y %)))
-(defn Var [x] #(x %))
-(defn Num [x] (fn [_] x))
-
-(def ast (Add (Var 'a) (Mul (Num 2) (Var 'b))))
-(def env '{a 3 b 4 c 5})
-(def result (ast env))
